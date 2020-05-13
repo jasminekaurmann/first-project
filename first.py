d04f3a0f90af67345f187c94884bbe44189e4906
jasminekaurmann <65140769+jasminekaurmann@users.noreply.github.com>
2020-05-13 13:25:29 -0400

    Update first.py

--- first.py
+++ first.py
@@ -1,5 +1,15 @@
-print('whattup')
-def x3(num):
-    return (num*2)
+import tkinter
+# init tk
+root = tkinter.Tk()
 
-print(x3(1))
+# create canvas
+myCanvas = tkinter.Canvas(root, bg="white", height=700, width=700)
+
+# draw arcs
+coord = 10, 10, 300, 300
+arc = myCanvas.create_arc(coord, start=0, extent=160, fill="pink")
+arv2 = myCanvas.create_arc(coord, start=160, extent=215, fill="lightblue")
+
+# add to window and show
+myCanvas.pack()
+root.mainloop()
